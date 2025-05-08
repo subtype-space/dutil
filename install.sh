5a3f001f6fb70f73d162c3edd9a19151602f1920
subtype <andrew.subowo@gmail.com>
2025-05-07 21:07:07 -0400

    add installer

--- install.sh
+++ install.sh
@@ -1,21 +1,19 @@
 #/bin/bash
 
 REPO_URL="https://github.com/subtype-space/dutil"
-INSTALL_DIR="$HOME/.dutil"
 BIN_NAME="dutil"
 SYMLINK_PATH="/usr/local/bin/$BIN_NAME"
 
 
 echo "Installing dutil..."
-
 echo "Updating dutil..." && git pull
 
-chmod +x "$INSTALL_DIR/dutil.sh"
+chmod +x ./dutil.sh
 
 if [ -L $SYMLINK_PATH ]; then
     rm "$SYMLINK_PATH"
 fi
 
-ln -s "$INSTALL_DIR/dutil.sh" "$SYMLINK_PATH" && echo "Created symlink at $SYMLINK_PATH" || echo "Failed to create symlink!"
+ln -s "./dutil.sh" "$SYMLINK_PATH" && echo "Created symlink at $SYMLINK_PATH" || echo "Failed to create symlink!"
 
 echo "🎉 dutil installed! Run it with: dutil"
